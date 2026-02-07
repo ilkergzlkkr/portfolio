7a289bce37fec5da645c341ba06dcdaeb0c93b81
ilkergzlkkr <guzelkokarilker@gmail.com>
2026-02-07 13:55:32 +0300

    feat: update landing page text

--- app/page.tsx
+++ app/page.tsx
@@ -35,9 +35,8 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building software to have fun. Currently student at Nişantaşı
-          University. I'm interested in full-stack development, app integration
-          and automation.
+          Software Engineer at Playable Factory, crafting innovative solutions across the full stack.
+          Passionate about app integration, automation, and building systems that scale.
         </h2>
       </div>
     </div>
